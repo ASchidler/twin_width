92560f9cc02faefe1b9b7e71a5bc126aa316e696
Andre Schidler <andre@schidler.at>
2020-12-30 20:07:54 +0100

    Fixed threading issue

--- experiments/random_graphs.py
+++ experiments/random_graphs.py
@@ -14,15 +14,12 @@ g_max_size = 40
 
 results = {}
 
-c_g_size = Value("i")
-c_p = Value("f")
-c_result = Value("i")
-
 pool_size = 4
 
 
-def compute_graph(i):
-    g = gnp_random_graph(g_size, cp)
+def compute_graph(args):
+    c_p, c_g_size = args
+    g = gnp_random_graph(c_g_size, c_p)
     components = list(nx.connected_components(g))
     tww = 0
     for c in components:
@@ -34,22 +31,21 @@ def compute_graph(i):
         enc = encoding.TwinWidthEncoding()
         result = enc.run(cg, Cadical, heuristic.get_ub(cg), check=False, verbose=False)
         tww = max(tww, result)
-    c_result.value += tww
+    return tww
 
 
 with open("random_results.csv", "w") as results_f:
     for g_size in range(g_min_size, g_max_size+1, g_increment):
-        c_g_size.value = g_size
         cp = increment
         results[g_size] = {}
         while cp < 1:
             with Pool(pool_size) as pool:
-                c_result.value = 0
-                c_p.Value = cp
-                pool.map(compute_graph, range(0, num_graphs))
+                c_total = 0
+                for c_tww in pool.imap_unordered(compute_graph, ((cp, g_size) for _ in range(0, num_graphs))):
+                    c_total += c_tww
                 pool.close()
                 pool.join()
-                results[g_size][cp] = c_result.value / num_graphs
+                results[g_size][cp] = c_total / num_graphs
                 print(f"{g_size} {cp} {results[g_size][cp]}")
                 results_f.write(f"{g_size};{cp};{results[g_size][cp]}{os.linesep}")
                 cp += increment
