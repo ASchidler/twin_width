14a082e9acdbf1e1836382cd6b5f47f9ed293426
Andre Schidler <andre@schidler.at>
2022-01-09 21:32:56 +0100

    More Cleanup

--- runner.py
+++ runner.py
@@ -13,7 +13,7 @@ from networkx.generators.lattice import grid_2d_graph
 from networkx.generators.random_graphs import gnp_random_graph
 import resource
 
-resource.setrlimit(resource.RLIMIT_AS, (16 * 1024 * 1024 * 1024, 16 * 1024 * 1024 * 1024))
+#resource.setrlimit(resource.RLIMIT_AS, (16 * 1024 * 1024 * 1024, 16 * 1024 * 1024 * 1024))
 
 #print(f"{tools.solve_grid2(7,7, 3)}")
 
@@ -26,11 +26,11 @@ g = parser.parse(instance)[0]
 # x = tools.find_modules(g)
 
 #
-def interrupt():
-    exit(1)
-
-timer = Timer(300, interrupt)
-timer.start()
+# def interrupt():
+#     exit(1)
+#
+# timer = Timer(300, interrupt)
+# timer.start()
 
 print(f"{len(g.nodes)} {len(g.edges)}")
 preprocessing.twin_merge(g)
@@ -39,7 +39,7 @@ if len(g.nodes) == 1:
     print("Done, width: 0")
     exit(0)
 #g = tools.prime_paley(73)
-g = grid_2d_graph(7, 7)
+#g = grid_2d_graph(7, 7)
 # g = gnp_random_graph(75, 0.5)
 # TODO: Deal with disconnected?
 ub = heuristic.get_ub(g)
@@ -50,8 +50,8 @@ ub = min(ub, ub2)
 start = time.time()
 enc = encoding.TwinWidthEncoding()
 #enc = encoding2.TwinWidthEncoding2(g)
-ub = 3
+
 cb = enc.run(g, slv.Cadical, ub)
 
 print(f"Finished, result: {cb}")
-timer.cancel()
+#timer.cancel()
