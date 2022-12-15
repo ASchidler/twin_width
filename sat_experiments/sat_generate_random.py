3aa882d3bf85552deae7f6ef1aa0c4a176533a8a
Andre Schidler <andre@schidler.at>
2022-12-15 13:45:41 +0100

    Added SAT instances

---
+++ sat_experiments/sat_generate_random.py
@@ -0,0 +1,14 @@
+import random
+
+import cnfgen
+
+variables = 15
+clauses = [15, 30, 45, 65, 80, 95, 110]
+ks = [2, 3, 5, 7, 10]
+
+for c_cl in clauses:
+    for c_k in ks:
+        for i in range(1, 11):
+            bla = cnfgen.RandomKCNF(c_k, variables, c_cl)
+            with open(f"sat_experiments/randoms/ur_{c_k}_{variables}_{c_cl}_{i}.cnf", "w") as outp:
+                outp.write(bla.dimacs())
